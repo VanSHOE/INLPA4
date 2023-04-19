96ea51edde4eba1f16dea63993676cabfea42cab
Rahul Garg <rahulgargofficial@gmail.com>
2023-04-19 05:39:29 +0530

    lil model structure

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import torchtext
 from torchtext.vocab import GloVe
 from tqdm import tqdm
 from datasets import load_dataset
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 GLOVE_DIM = 200
 
@@ -57,3 +60,35 @@ vocabulary = torchtext.vocab.build_vocab_from_iterator([[intersection] for inter
                                                        "<unk>", "<eos>", "<sos>", "<pad>"], special_first=True)
 ic(vocabulary.get_itos()[0:30])
 print("Vocabulary size: ", len(vocabulary))
+
+
+class ELMo(torch.nn.Module):
+    def __init__(self, h, vocab, glEmbed):
+        super().__init__()
+        self.vocab = vocab
+        self.glove_dim = GLOVE_DIM
+        self.hidden_size = h
+        self.vocab_size = len(vocab)
+        self.embedding = torch.nn.Embedding.from_pretrained(glEmbed)
+        self.f1 = torch.nn.LSTM(self.glove_dim, h, batch_first=True)
+        self.b1 = torch.nn.LSTM(self.glove_dim, h, batch_first=True)
+
+        self.f2 = torch.nn.LSTM(h, h, batch_first=True)
+        self.b2 = torch.nn.LSTM(h, h, batch_first=True)
+
+        self.final = torch.nn.Linear(h, self.vocab_size)
+        self.finalWeights = torch.nn.Parameter(torch.randn((1, 3)).to(device))
+
+    def forward(self, x, mode="train"):
+        x = self.embedding(x)
+
+        f1, _ = self.f1(x)
+        b1, _ = self.b1(torch.flip(x, [1]))
+        b1 = torch.flip(b1, [1])
+
+        f2, _ = self.f2(f1)
+        b2, _ = self.b2(torch.flip(b1, [1]))
+        b2 = torch.flip(b2, [1])
+
+        if mode == "train":
+            return self.final(f2), self.final(b2)
